f2ddf17c66ff90acab4919bd2335cc81963b959b
Jeremy Moreau <jeremyfs.moreau@gmail.com>
2018-06-04 14:08:56 +0200

    first commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatInputModule
+  ],
+  exports:[MatInputModule],
+  declarations: []
+})
+export class MaterialModule { }
