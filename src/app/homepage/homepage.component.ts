1d5a51c3f9cc2ee558030abb4b1e29ce22f12a54
Jeremy Moreau <jeremyfs.moreau@gmail.com>
2018-06-07 16:06:07 +0200

    avancee

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SpacexApiService } from '../Services/spacex-api.service';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-homepage',
@@ -7,15 +9,21 @@ import { SpacexApiService } from '../Services/spacex-api.service';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  nextLaunch: Launch[];
+  nextLaunch: Launch;
 
   constructor(private spacexApi: SpacexApiService) {
-    this.spacexApi.getNextLaunch<Launch[]>().subscribe(data => {
+    this.spacexApi.getNextLaunch().subscribe(data => {
       this.nextLaunch = data;
       console.log(this.nextLaunch);
     });
-  }
+  }  
+
   ngOnInit() {
-  }
+    interval(1000).pipe(
+      map((x) => {  
+
+      })
+    );
+  } 
 
 }
